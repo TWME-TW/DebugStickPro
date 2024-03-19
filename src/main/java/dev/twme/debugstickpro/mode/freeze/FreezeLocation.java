6d8690b7ef71b8b95a5307140d0235e4d5914492
TWME <65117253+TWME-TW@users.noreply.github.com>
2024-03-20 00:07:37 +0800

    修復插件在多世界時的潛在問題

--- src/main/java/dev/twme/debugstickpro/mode/freeze/FreezeLocation.java
+++ src/main/java/dev/twme/debugstickpro/mode/freeze/FreezeLocation.java
@@ -2,19 +2,24 @@ package dev.twme.debugstickpro.mode.freeze;
 
 import org.bukkit.Location;
 
+import java.util.UUID;
+
 public class FreezeLocation {
     private final int x;
     private final int y;
     private final int z;
     private final Location location;
+    private final UUID worldUUID;
 
 
     public FreezeLocation(Location location) {
         this.location = location;
+        this.worldUUID = location.getWorld().getUID();
         this.x = (int) location.getX();
         this.y = (int) location.getY();
         this.z = (int) location.getZ();
     }
+
     public Location getLocation() {
         return location;
     }
@@ -24,14 +29,15 @@ public class FreezeLocation {
         if (this == o) return true;
         if (!(o instanceof FreezeLocation thisData)) return false;
 
-        return thisData.x == this.x && thisData.y == this.y && thisData.z == this.z;
+        return thisData.x == this.x && thisData.y == this.y && thisData.z == this.z && thisData.worldUUID.equals(this.worldUUID);
     }
 
     @Override
     public int hashCode() {
-        int result =  x;
-        result = 31 * result +  y;
-        result = 31 * result +  z;
+        int result = worldUUID.hashCode();
+        result = 30 * result + x;
+        result = 31 * result + y;
+        result = 31 * result + z;
         return result;
     }
 }
